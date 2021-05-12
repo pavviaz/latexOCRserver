6a5a443966cf72be9ac107e58d0a8ffd7cd18cee
pavviaz <sha.cehca@yandex.ru>
2021-05-13 00:33:51 +0300

    change

--- pix2tex.py
+++ pix2tex.py
@@ -1,3 +1,13 @@
+import sys
+import subprocess
+import pkg_resources
+required = {"x-transformers"}
+installed = {pkg.key for pkg in pkg_resources.working_set}
+missing = required - installed
+if missing:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install',*missing])
+
+
 from dataset.dataset import test_transform
 from PIL import Image
 import argparse
